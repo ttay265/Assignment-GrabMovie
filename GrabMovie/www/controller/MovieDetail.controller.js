08402ad2ea82b7b5e891f755ad71533d0335f682
ttay265 <tuannq265@gmail.com>
2017-07-07 07:47:37 +0700

    tuannq a: moviDetail view + contr

--- GrabMovie/www/controller/MovieDetail.controller.js
+++ GrabMovie/www/controller/MovieDetail.controller.js
@@ -3,42 +3,21 @@ sap.ui.define([
 ], function(BaseController) {
 	"use strict";
 
-	return BaseController.extend("GrabMovie.controller.Main", {
+	return BaseController.extend("GrabMovie.controller.MovieDetail", {
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf GrabMovie.view.Main
+		 * @memberOf GrabMovie.GrabMovie.www.view.MovieDetail
 		 */
-		onInit: function() {
+		//	onInit: function() {
+		//
+		//	},
 
-		},
-
-		navPage: function(oEvent) {
-			var sourceId = oEvent.getSource().getIdForLabel();
-			var pageIndex = this.getGlobalModel().getProperty("/CurrentPage");
-			switch (sourceId) {
-				case this.getSId("btnBooking"):
-					pageIndex = 1;
-					break;
-				case this.getSId("btnTickets"):
-					pageIndex = 2;
-					break;
-				case this.getSId("btnProfile"):
-					pageIndex = 3;
-					break;
-			}
-			this.getGlobalModel().setProperty("/CurrentPage", pageIndex, null, true);
-		},
-		toDetail: function(oEvent) {
-			this.getRouter().navTo("movieDetail", {
-				movieId: 1
-			}, false);
-		}
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf GrabMovie.view.Main
+		 * @memberOf GrabMovie.GrabMovie.www.view.MovieDetail
 		 */
 		//	onBeforeRendering: function() {
 		//
@@ -47,7 +26,7 @@ sap.ui.define([
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf GrabMovie.view.Main
+		 * @memberOf GrabMovie.GrabMovie.www.view.MovieDetail
 		 */
 		//	onAfterRendering: function() {
 		//
@@ -55,7 +34,7 @@ sap.ui.define([
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf GrabMovie.view.Main
+		 * @memberOf GrabMovie.GrabMovie.www.view.MovieDetail
 		 */
 		//	onExit: function() {
 		//
